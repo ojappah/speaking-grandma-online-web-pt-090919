58c2925fe4b999624a24990d240fee6dcf6196a6
generous-vector-9581 <ojappah@gmail.com>
2019-09-28 00:35:51 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,15 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(string)
+  if string != string.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif string.upcase != "I LOVE YOU GRANDMA!"
+    return "NO, NOT SINCE 1938!"
+else  
+  string.upcase && "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
+end
+end
+  
+  
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
