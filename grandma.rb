afc426a4604db50ec1efbcefb82b66effda0181a
generous-vector-9581 <ojappah@gmail.com>
2019-09-28 00:21:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,17 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(grandma)
+  response = grandma
+  if response == response.downcase
+return "HUH?! SPEAK UP, SONNY!"
+elsif 
+  response == response.upcase && "I LOVE YOU GRANDMA!"
+  return "NO, NOT SINCE 1938!"
+else  
+  response == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
+end
+end
+  
+  
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
